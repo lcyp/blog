c03baba4046bfa4f328486a0aab4f9316152dc7f
lcy <2089117474@qq.com>
2024-11-14 13:14:09 +0800

    修改路由

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,15 +3,15 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/blog/zh/",
+  base: "/blog/",
 
   locales: {
-    "/": {
+    "/en/": {
       lang: "en-US",
       title: "Blog Demo",
       description: "A blog demo for vuepress-theme-hope",
     },
-    "/zh/": {
+    "/": {
       lang: "zh-CN",
       title: "博客演示",
       description: "vuepress-theme-hope 的博客演示",
