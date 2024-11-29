1a8b32e1ce211e5e01d0ded3b9c7095f47ddc189
lcy <2089117474@qq.com>
2024-11-29 09:40:40 +0800

    文档

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -2,18 +2,27 @@ import { sidebar } from "vuepress-theme-hope";
 
 export const zhSidebar = sidebar({
   "/zh/": [
-     "",
-     {
+    "",
+    {
       text: "文章",
       icon: "book",
       prefix: "posts/",
-      children:[
+      children: [
+        {
+          text: "JVM",
+          icon: "/icons/lineicons--vmware.png",
+          collapsible: true,
+          prefix: "JVM/",
+          children: [
+            "1",
+          ],
+        },
         {
           text: "Redis",
           icon: "/icons/devicon--redis.png",
           collapsible: true,
           prefix: "Redis/",
-          children:[
+          children: [
             "Redis",
           ],
         },
@@ -22,60 +31,60 @@ export const zhSidebar = sidebar({
           icon: "/icons/mynaui--face-id.png",
           prefix: "面试求职/",
           collapsible: true,
-          children:[
-           {
-             text: "java基础知识",
-             icon: "/icons/devicon--java.png",
-             collapsible: true, 
-             prefix: "java基础知识/",
-             children:[
-               "1",
-               "2",
-             ],
-           },
-           {
-            text: "JVM",
-            icon: "/icons/lineicons--vmware.png",
-            collapsible: true, 
-            prefix: "JVM/",
-            children:[
-              "1",
-              "2",
-              "3",
-              "4",
-              "5",
-            ],
-          },
-          {
-            text: "MySQL",
-            icon: "/icons/logos--mysql.png",
-            collapsible: true, 
-            prefix: "Mysql/",
-            children:[
-              "1",
-            ],
-          },
-          {
-            text: "Redis",
-            icon: "/icons/devicon--redis.png",
-            collapsible: true, 
-            prefix: "Redis/",
-            children:[
-              "1",
-            ],
-          },
-          {
-            text: "Spring",
-            icon: "/icons/devicon--spring.png",
-            collapsible: true, 
-            prefix: "Spring知识/",
-            children:[
-              "1",
-              "2",
-              "3",
-              "4",
-            ],
-          },
+          children: [
+            {
+              text: "java基础知识",
+              icon: "/icons/devicon--java.png",
+              collapsible: true,
+              prefix: "java基础知识/",
+              children: [
+                "1",
+                "2",
+              ],
+            },
+            {
+              text: "JVM",
+              icon: "/icons/lineicons--vmware.png",
+              collapsible: true,
+              prefix: "JVM/",
+              children: [
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+              ],
+            },
+            {
+              text: "MySQL",
+              icon: "/icons/logos--mysql.png",
+              collapsible: true,
+              prefix: "Mysql/",
+              children: [
+                "1",
+              ],
+            },
+            {
+              text: "Redis",
+              icon: "/icons/devicon--redis.png",
+              collapsible: true,
+              prefix: "Redis/",
+              children: [
+                "1",
+              ],
+            },
+            {
+              text: "Spring",
+              icon: "/icons/devicon--spring.png",
+              collapsible: true,
+              prefix: "Spring知识/",
+              children: [
+                "1",
+                "2",
+                "3",
+                "4",
+              ],
+            },
           ],
         }
       ]
